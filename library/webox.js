2060f356d852bff1d592664cbe1de2932a104d5f
若海 <mail@anrip.com>
2016-01-12 12:58:15 +0800

    update webox log

--- library/webox.js
+++ library/webox.js
@@ -9,9 +9,6 @@ var http = require('http');
 var url  = require('url');
 var fs   = require('fs');
 
-var wbmime = require('./wbmime');
-var dfmine = 'application/octet-stream';
-
 // set title
 process.title = 'HTTP 服务器';
 
@@ -21,11 +18,16 @@ process.env.INIT_CWD = process.cwd();
 // exit with 0 or 1
 var failed = false;
 process.once('exit', function(code) {
-    console.log('服务已停止');
+    webox.log('服务已停止');
     if(code === 0 && failed) {
         process.exit(1);
     }
 });
+
+/////////////////////////////////////////////////////////////
+
+var wbmime = require('./wbmime');
+var dfmine = 'application/octet-stream';
 
 // parse config
 
@@ -43,7 +45,7 @@ var webox = http.createServer(function(request, response) {
     var pathTemp = request.url.match(/\/$|^\/\?/) ? '/index.html' : request.url;
     var pathName = url.parse(pathTemp).pathname;
     var realPath = path.join(wbroot, pathName);
-    console.log('请求源文件 ' + realPath);
+    webox.log('请求源文件 ' + realPath);
     //尝试查找文件
     fs.exists(realPath, function(exists) {
         //找不到文件
@@ -75,19 +77,49 @@ var webox = http.createServer(function(request, response) {
     });
 });
 
+webox.log = function() {
+    var arg1 = ['[' + dateFormat('hh:mm:ss') + ']', 'Webox -'];
+    var arg2 = Array.prototype.slice.call(arguments);
+    console.log.apply(null, arg1.concat(arg2));
+};
+
 webox.on('error', function (e) {
     if(e.code == 'EADDRINUSE') {
-        console.log('端口已被占用', listen[0], listen[1], '\n');
-        console.log('尝试新的端口', listen[0], ++listen[1]);
+        webox.log('端口已被占用', listen[0], listen[1], '\n');
+        webox.log('尝试新的端口', listen[0], ++listen[1]);
         webox.listen(listen[1], listen[0], 1024);
     }
 });
 
 webox.on('listening', function () {
     var host = listen[0] === '0.0.0.0' ? '127.0.0.1' : listen[0];
-    console.log('服务启动成功', 'http://' + host + ':' + listen[1], '\n');
+    webox.log('服务启动成功', 'http://' + host + ':' + listen[1], '\n');
 });
 
 // start server
 
 webox.listen(listen[1], listen[0], 1024);
+
+/////////////////////////////////////////////////////////////
+
+function dateFormat(fmt) {
+    var d = new Date();
+    var o = {
+        'M+': d.getMonth() + 1,//月
+        'd+': d.getDate(),//日
+        'h+': d.getHours(),//时
+        'm+': d.getMinutes(), //分
+        's+': d.getSeconds(), //秒
+        'q+': Math.floor((d.getMonth() + 3) / 3),//季度
+        'S': d.getMilliseconds()//毫秒
+    };
+    if(/(y+)/.test(fmt)) {//年
+        fmt = fmt.replace(RegExp.$1, (d.getFullYear() + '').substr(4 - RegExp.$1.length));
+    }
+    for(var k in o) {
+        if(new RegExp('(' + k + ')').test(fmt)) {
+            fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (('00' + o[k]).substr(('' + o[k]).length)));
+        }
+    }
+    return fmt;
+}
